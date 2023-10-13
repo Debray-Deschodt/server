4f766a66e6efca25528dd8718ed141e8508c8690
Debray-Deschodt <debraydeschodtpaul@gmail.com>
2023-10-13 17:02:16 +0200

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  apps : [{
+    name: "Uchronie",
+    script: "./bin/www",
+    mode: "cluster",
+    autorestart: true,
+    watch: true,
+    instances: "max" ,
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }, 
+//   {
+//     name: "worker",
+//     script: "./bin/www"
+//   }
+]}
+
+
+// module.exports = {
+//   apps : [{
+//     script: './bin/www',
+//     watch: '.'
+//   }, 
+//   // {
+//   //   script: './service-worker/',
+//   //   watch: ['./service-worker']
+//   // }
+// ],
+
+//   // deploy : {
+//   //   production : {
+//   //     user : 'SSH_USERNAME',
+//   //     host : 'SSH_HOSTMACHINE',
+//   //     ref  : 'origin/master',
+//   //     repo : 'GIT_REPOSITORY',
+//   //     path : 'DESTINATION_PATH',
+//   //     'pre-deploy-local': '',
+//   //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+//   //     'pre-setup': ''
+//   //   }
+//   // }
+// };
--- readme.md
+++ readme.md
@@ -6,9 +6,12 @@ kill -9 //PID
 
 where you replace //PID by the process ID.
 
-ssh ubuntu@149.202.54.101 -p 61487
+ssh debian@149.202.54.101 -p 61487
+ssh debian@149.202.54.101 -p 61487
 ytjDd4Xcc3g8
 
-every tree month : sudo chown ubuntu -R /etc/letsencrypt
+every tree month : sudo chown debian -R /etc/letsencrypt
+sudo lsof -i :80
+sudo service apache2 stop
 it's for permission access denied
 there's maybe better solutions
