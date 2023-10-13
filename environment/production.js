19caa828b44449271d0c28e3c33a983e9ef380c7
Debray-Deschodt <debraydeschodtpaul@gmail.com>
2023-10-13 15:49:26 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ const path = require("path")
 
 module.exports = {
     dbUrl : 'mongodb+srv://admin:vz3409aTHKMEZcgO%3F%2FyPCT@uchronie.5j5gecp.mongodb.net/',
-    cert : '/etc/letsencrypt/live/www.uchronie.ovh/fullchain.pem',
-    key : '/etc/letsencrypt/live/www.uchronie.ovh/privkey.pem' 
+    cert : '/etc/letsencrypt/live/uchronie.ovh/fullchain.pem',
+    key : '/etc/letsencrypt/live/uchronie.ovh/privkey.pem' 
 }
--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ kill -9 //PID
 
 where you replace //PID by the process ID.
 
-ssh ubuntu@149.202.54.101
+ssh ubuntu@149.202.54.101 -p 61487
 ytjDd4Xcc3g8
 
 every tree month : sudo chown ubuntu -R /etc/letsencrypt
